842c1cb1ae8970cda6beee8c38d00fc045d1e2d4
Ashish Kumar <ashishkumarcsee@gmail.com>
2026-08-19 15:25:01 +0530

    inverted right angle triangle pattern

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int n = 5;
+        int n = 9;
 
         for(int row = 1; row <= n; row++){
-            for(int spaces = 1; spaces <=(n-row); spaces++){
-                System.out.print("  ");
-            }
 
-            for(int col = 1; col <= 5; col++){
+            for(int col = 1; col <= (n-row)+1; col++){
                 System.out.print("* ");
             }
 
