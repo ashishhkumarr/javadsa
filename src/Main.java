aecdc2bf85d10141126f427449a4a1b6c1e86cc0
Ashish Kumar <ashishkumarcsee@gmail.com>
2026-08-19 17:45:39 +0530

    hollow pyramid pattern

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,45 @@
 public class Main {
     public static void main(String[] args) {
 
-        int n = 9;
+        int n = 7;
 
-        for(int row = 1; row <= n; row++){
+        for (int row = 1; row <= n; row++) {
 
             //pattern - hollow triangle
 
-            if(row == 1 || row == 2 || row == n) {
-                for (int col = 1; col <= row; col++) {
-                    System.out.print("* ");
-                }
+            if (row == 1) {
 
-            } else {
-
-                System.out.print("* ");
-
-                for (int col = 1; col <= row-2; col++) {
+                for (int space = 1; space <= n - row; space++) {
                     System.out.print("  ");
                 }
 
                 System.out.print("* ");
 
+            } else if (row <= n - 1) {
+
+                // space
+                for (int space = 1; space <= n - row; space++) {
+                    System.out.print("  ");
+                }
+
+                //start
+                System.out.print("* ");
+
+                //middle
+                for (int middle = 1; middle <= 2 * row - 3; middle++) {
+                    System.out.print("  ");
+                }
+
+                //end
+                System.out.print("* ");
+
+            } else {
+                for (int col = 1; col <= 2*n-1; col++) {
+                    System.out.print("* ");
+                }
             }
 
             System.out.println();
         }
-
     }
 }
