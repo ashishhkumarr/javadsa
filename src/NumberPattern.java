27f97f53808fd0fc3261c1a02cacc203002dd323
Ashish Kumar <ashishkumarcsee@gmail.com>
2026-08-20 15:24:47 +0530

    floyd's triangle pattern

--- src/NumberPattern.java
+++ src/NumberPattern.java
@@ -2,10 +2,24 @@ public class NumberPattern {
         public static void main(String[] args) {
 
             int n = 5;
+
+//            // --------- Right Angle Triangle Number Pattern --------- //
+//            for(int row = 1; row <= n; row++){
+//
+//                for(int col = 1; col <= row; col++){
+//                    System.out.print(col + " ");
+//                }
+//
+//                System.out.println();
+//            }
+
+            // Floyd's Triangle Pattern
+            int count = 1;
             for(int row = 1; row <= n; row++){
 
                 for(int col = 1; col <= row; col++){
-                    System.out.print(col + " ");
+                    System.out.print(count + " ");
+                    count++;
                 }
 
                 System.out.println();
