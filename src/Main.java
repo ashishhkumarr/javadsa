4215877be1c2104b33302e5fc41ab93f680be3cd
Ashish Kumar <ashishkumarcsee@gmail.com>
2026-08-22 00:04:00 +0530

    Number Pyramid Pattern

--- src/Main.java
+++ src/Main.java
@@ -37,24 +37,42 @@ public class Main {
 //
 //        }
 
+//        // ------------- **************** ------------- //
+//
+//        for(int row = 1; row <= n; row++){
+//
+//            //spaces
+//
+//            for(int space = 1; space <= n-row; space++){
+//                System.out.print("  ");
+//            }
+//
+//            for(int col = 1; col <= row; col++){
+//                System.out.print(col + " ");
+//            }
+//
+//            int decrementRow = row -1;
+//            for(int part3 = 1; part3 <= row-1; part3++){
+//                    System.out.print(decrementRow + " ");
+//                    decrementRow--;
+//            }
+//
+//            System.out.println();
+//        }
+
         // ------------- **************** ------------- //
+
+        // Number Pyramid Pattern //
 
         for(int row = 1; row <= n; row++){
 
             //spaces
-
-            for(int space = 1; space <= n-row; space++){
+            for( int spaces = 1; spaces <= n-row; spaces++){
                 System.out.print("  ");
             }
 
-            for(int col = 1; col <= row; col++){
-                System.out.print(col + " ");
-            }
-
-            int decrementRow = row -1;
-            for(int part3 = 1; part3 <= row-1; part3++){
-                    System.out.print(decrementRow + " ");
-                    decrementRow--;
+            for(int col = 1; col <= 2*row-1; col++){
+                System.out.print(row + " ");
             }
 
             System.out.println();
